67dd1bfc5f81285c5b6d263e645b23bef555cfa3
unknown <mohanad.13.93@gmail.com>
2025-01-07 18:25:26 +0100

    add register login logout user

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import logout_route
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', include('profiles.urls'))
+    path('profiles/', include('profiles.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', 
+         include('dj_rest_auth.registration.urls')),
 ]
