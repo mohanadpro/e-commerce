511ff12189f8df6fe7a16c3d50aec3f6ffe128d6
unknown <mohanad.13.93@gmail.com>
2025-01-08 13:19:32 +0100

    add database to env file and gunicorn to Procfile

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 if os.path.exists('env.py'):
@@ -90,14 +91,19 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+     DATABASE_URL = os.environ.get('DATABASE_URL')
+     DATABASES = {
+         'default': dj_database_url.config(default=DATABASE_URL)
+     }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
