9700081d8d5bf7e41ea78b9d037ac1a1be8b308a
unknown <mohanad.13.93@gmail.com>
2025-01-08 14:32:35 +0100

    update urls add root route

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ebuy-d1c37ed2b301.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1","ebuy-d1c37ed2b301.herokuapp.com"]
 
 
 # Application definition
