00ae2c4018bcd34112ddf1b203dec36029d86c10
unknown <mohanad.13.93@gmail.com>
2025-01-09 19:54:04 +0100

    update settings

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        else 'rest_framework_simplejwt.authentication.JWTAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
