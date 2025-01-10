5eda454b41e1fbbd6ae9a39b4a0055f099d9b6a5
unknown <mohanad.13.93@gmail.com>
2025-01-10 08:57:25 +0100

    update urls

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -21,10 +21,11 @@ from .views import logout_route, root_route
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
-    path('profiles/', include('profiles.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', 
          include('dj_rest_auth.registration.urls')),
+    path('profiles/', include('profiles.urls')),
+
 ]
