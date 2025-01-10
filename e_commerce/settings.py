dd1e7546f4463f8d3c0b16c577ea926a558b8cc0
unknown <mohanad.13.93@gmail.com>
2025-01-10 09:53:46 +0100

    update setting

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -72,14 +72,16 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN')
     ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+if 'CLIENT_ORIGIN' in os.environ:
+    print('CLIENT_ORIGIN')
 
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https:\/\/.*\.codeinstitute-ide\.net$",
+    ]
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
