50b0846311d961f3f7fcbbabc30323ec0003e9a0
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-18 16:58:23 +0000

    update urls.py orderDetails/<int:pk>/ to urls

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import OrderList, OrderDetails
 urlpatterns = [
     path('', OrderList.as_view()),
-    path('<int:id>/', OrderDetails.as_view()),
+    path('orderDetails/<int:pk>/', OrderDetails.as_view()),
 ]
