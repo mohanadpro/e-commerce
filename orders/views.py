cc48254ad2dbccc66cde190e0b55e92b7bf94c58
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-18 17:03:21 +0000

    add orders.views add delivery_place to the saved order and get orderdetails by id

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Order
 from .models import Order_Product
 from .serializers import OrderSerializer, OrderDetailsSerializer
@@ -6,34 +7,41 @@ from e_commerce.permissions import IsOwnerOrReadOnly
 import json
 # Create your views here.
 class OrderList(generics.ListCreateAPIView):
+
     # permission_classes = [IsOwnerOrReadOnly]
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+
+
+    def list(self, request):
+        queryset = Order.objects.filter(customer=self.request.user)
+        serializer = OrderSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         if serializer.is_valid():
             order = serializer.save()
             cart = self.request.POST.get('cart')
+            delivery_place = self.request.POST.get('delivery_place')
             products = json.loads(cart)
             for prod in products:
                 try:
-                    prod['order'] = 5
+                    prod['order'] = order.id
                     saved_product = OrderDetailsSerializer(data=prod)
                     if saved_product.is_valid():
-                        print('serializer is valid')
-                        print(saved_product)
                         saved_product.save()
                     else:
                         print('serializer is not valid')
-                    # print('success')
                 except ValidationError as e:
                     print(str(e))
                 except Exception as ex:
                     print(str(ex))
 
-class OrderDetails(generics.RetrieveAPIView):
+class OrderDetails(generics.ListCreateAPIView):
     serializer_class = OrderDetailsSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    lookup_url_kwarg = 'id'
-    print(lookup_url_kwarg)    
-    queryset = Order_Product.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        order_id = self.kwargs['pk']
+        queryset = Order_Product.objects.filter(order=order_id)
+        serializer = OrderDetailsSerializer(queryset, many=True)
+        return Response(serializer.data)
