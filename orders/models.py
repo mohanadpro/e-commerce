9c2a39ef219583a6fe4bfbfd45c0919b8cc7ef30
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-18 16:54:05 +0000

    update Order model add delivery_place field

--- orders/models.py
+++ orders/models.py
@@ -7,6 +7,7 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.FloatField()
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_place = models.CharField(max_length=250, blank=True)
 
 class Order_Product(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
