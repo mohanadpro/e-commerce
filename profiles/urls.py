5dc6d60ecc1e3c95910017e802763dc6f8dd647d
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-19 10:41:11 +0000

    update urls.py remove /  from ProfileDetails url

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProfileList, ProfileDetails
 urlpatterns = [
     path('', ProfileList.as_view()),
-    path('<int:pk>/', ProfileDetails.as_view()),
+    path('<int:pk>', ProfileDetails.as_view()),
 ]
