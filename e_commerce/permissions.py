02a39fb64c5ee45be707b685fbe344d03934416f
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-20 15:30:25 +0000

    update permissions.py add IsOwner permission to allow just owner user to get its orders

--- e_commerce/permissions.py
+++ e_commerce/permissions.py
@@ -5,3 +5,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+class IsOwnerOrders(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        print(obj.customer, request.user)
+        return obj.customer == request.user
