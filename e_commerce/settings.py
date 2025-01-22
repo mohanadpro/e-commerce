f6a4f868a1c7144a36f5ee328b23ae7e3153478d
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-22 23:07:32 +0000

    update setting.py CORS_ALLOWED_ORIGINS

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -75,7 +75,7 @@ ALLOWED_HOSTS = [
         # os.environ.get('CLIENT_ORIGIN')
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        "https://mohanadpro.github.io/e-commerce-frontend/"
+        "https://mohanadpro.github.io/e-commerce-frontend"
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
