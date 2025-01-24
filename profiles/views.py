72d506eb7d98064342a6ca1255bfa5f5d1ae918c
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-24 10:08:46 +0000

    update profile write python code in standard way

--- profiles/views.py
+++ profiles/views.py
@@ -3,13 +3,16 @@ from .models import Profile
 from .serializers import ProfileSerializer
 from e_commerce.permissions import IsOwner
 
+
 # Create your views here.
 class ProfileList(generics.ListCreateAPIView):
     permission_classes = [IsOwner]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
 
 class ProfileDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
