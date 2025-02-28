cb7d8d28c16b91722c901963bc139a915d638df2
unknown <mohanad.13.93@gmail.com>
2025-02-28 17:52:42 +0100

    update e-commerce/permissions.py add 2 new permissions IsAdminOrReadOnly and IsAdminOrOwner

--- e_commerce/permissions.py
+++ e_commerce/permissions.py
@@ -6,6 +6,18 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         return obj.owner == request.user
 
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == permissions.IsAdminUser
+
+class IsAdminOrOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (request.user == permissions.IsAdminUser)
+
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
