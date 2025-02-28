105ab381e65b21a3ba7afa78e25e87ee35e41c4b
unknown <mohanad.13.93@gmail.com>
2025-02-28 17:00:06 +0100

    update urls.py add two urls 'dj-rest-auth/login/ and dj-rest-auth/user/ to customize login process

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -17,16 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import logout_route, root_route
-
+from accounts.views import CustomLoginView, UserView
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     # our logout route has to be above the default one to be matched first
     path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/login/', CustomLoginView.as_view(), name='custom_login'),
+    path('dj-rest-auth/user/', UserView.as_view(), name='custom_user'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('check-user-admin/', include('accounts.urls')),
     path('profiles/', include('profiles.urls')),
     path('categories/', include('categories.urls')),
     path('products/', include('products.urls')),
