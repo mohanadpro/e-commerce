6d6c8947728e601e52a81601cab906c8f41bd288
unknown <mohanad.13.93@gmail.com>
2025-02-28 17:41:19 +0100

    update account serializers.py add profile_id property

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,8 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_id = serializers.ReadOnlyField(source='profile.id')
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile_image']
+        fields = ['pk', 'username', 'email', 'first_name', 'last_name', 'profile_image', 'profile_id']
