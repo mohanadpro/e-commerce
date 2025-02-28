3430af2416457aaf16d355fadf0d8ef30b6c99fa
unknown <mohanad.13.93@gmail.com>
2025-02-28 17:11:22 +0100

    update orders urls.py add allOrders url to get all orders when user is admin

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderList, OrderDetails
+from .views import OrderList, OrderDetails, OrderlistAllOrders
 urlpatterns = [
     path('', OrderList.as_view()),
+    path('allOrders/', OrderlistAllOrders.as_view()),
     path('orderDetails/<int:pk>/', OrderDetails.as_view()),
 ]
