391474f79471a091adfe5c569c732b01d3ed7fa4
unknown <mohanad.13.93@gmail.com>
2025-03-02 12:22:35 +0100

    update products serializers add readonly column category_name

--- products/serializers.py
+++ products/serializers.py
@@ -3,11 +3,14 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.ReadOnlyField(source='category.name')
+    
     class Meta:
         model = Product
         fields = [
             'id', 'name', 'category',
             'created_at', 'updated_at',
             'image', 'price', 'description',
-            'color', 'size', 'genders'
+            'color', 'size', 'genders',
+            'category_name'
             ]
