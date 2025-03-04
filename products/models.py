4ef0f21dc513d51b7eb3175fe1a552686032ff22
unknown <mohanad.13.93@gmail.com>
2025-03-04 14:53:55 +0100

    update product name field and category name field to be unique

--- products/models.py
+++ products/models.py
@@ -55,7 +55,7 @@ class Product(models.Model):
         ('Unisex', 'Unisex'),
         ('Youth', 'youth'),
     ]
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
