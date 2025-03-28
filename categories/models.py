9ec6ac99dc65c479a152f36df7cc9657eae65ecc
unknown <mohanad.13.93@gmail.com>
2025-03-28 13:41:32 +0100

    update category model add created_at field to order category by created date desc

--- categories/models.py
+++ categories/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Category(models.Model):
 
     name = models.CharField(max_length=75, unique=True)
-
+    created_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_at']
     def __str__(self):
         return self.name
