366ffbee6d6ffe3e26f26cfbdd5231fba5f2b059
unknown <mohanad.13.93@gmail.com>
2025-03-28 13:44:15 +0100

    update orders.urls add new url to delete order history

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderList, OrderDetails, OrderlistAllOrders
+from .views import OrderList, OrderDetails, OrderlistAllOrders, OrderDestroy
 urlpatterns = [
     path('', OrderList.as_view()),
+    path('<int:pk>/', OrderDestroy.as_view()),
     path('allOrders/', OrderlistAllOrders.as_view()),
     path('orderDetails/<int:pk>/', OrderDetails.as_view()),
 ]
